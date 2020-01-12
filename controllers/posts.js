1e17c457cd7d92d8373e2ef2010862b895ebcf9c
Jonas Rodon <jonascoder@gmail.com>
2020-01-12 20:50:57 +0100

    Posts Index revisited

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,7 @@ const Post = require('../models/post');
 module.exports = {
     // Posts Index
     async getPosts(req, res, next) {
-        let posts = Post.find({});
+        let posts = await Post.find({});
         res.render('posts/index', { posts });
     },
     // Posts New
