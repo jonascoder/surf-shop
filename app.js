e1246427147fa09ec9baaa5a84bb3bc7e0d9c166
Jonas Rodon <jonascoder@gmail.com>
2020-01-12 13:20:35 +0100

    Login and Logout Routes

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.use(session({
     secret: 'hang ten dude!',
     resave: false,
     saveUninitialized: true
-}))
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(User.createStrategy());
-
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
