4c370f0e142d51bcd47222e5533f1fcf1f5ba421
Jonas Rodon <jonascoder@gmail.com>
2020-01-12 13:41:59 +0100

    Refactor /login and /logout Routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-
+const passport = require('passport');
 module.exports = {
     async postRegister(req, res, next) {
         const newUser = new User({
@@ -9,6 +9,16 @@ module.exports = {
         });
 
         await User.register(newUser, req.body.password);
+        res.redirect('/');
+    },
+    postLogin(req, res, next) {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login'
+        })(req, res, next);
+    },
+    getLogout(req, res, next) {
+        req.logout();
         res.redirect('/');
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const { postRegister } = require('../controllers');
+const { postRegister, postLogin, getLogout } = require('../controllers');
 const { errorHandler } = require('../middleware');
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -22,16 +21,10 @@ router.get('/login', (req, res, next) => {
 });
 
 /* POST /login */
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}));
+router.post('/login', postLogin);
 
 /* GET /logout */
-router.get('/logout', (req, res, next) => {
-    req.logout();
-    res.redirect('/');
-});
+router.get('/logout', getLogout);
 
 /* GET /profile */
 router.get('/profile', (req, res, next) => {
