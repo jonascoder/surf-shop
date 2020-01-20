d2d2c30c00c8e568dcff4e6a1e45595b228a91ff
Jonas Rodon <jonascoder@gmail.com>
2020-01-20 23:32:26 +0100

    Reviews - Initial Setup

--- models/review.js
+++ models/review.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ReviewSchema = new Schema({
     body: String,
+    rating: Number,
     author: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-/* GET index       /posts/:id/reviews */
-router.get('/', (req, res, next) => {
-    res.send('INDEX /posts/:id/reviews')
-});
+
 /* POST reviews create     /posts/:id/reviews */
 router.post('/', (req, res, next) => {
     res.send('CREATE /posts/:id/reviews')
 });
-/* GET reviews edit        /posts/:id/reviews/:review_id/edit */
-router.get('/:review_id/edit', (req, res, next) => {
-    res.send('EDIT /posts/:id/reviews/:review_id/edit');
-});
+
 /* PUT reviews update      /posts/:id/reviews/:review_id */
 router.put('/:review_id', (req, res, next) => {
     res.send('UPDATE /posts/:id/reviews/:review_id')
