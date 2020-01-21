89dbd915ba8de7749a7f4541d1168c2a1744eccb
Jonas Rodon <jonascoder@gmail.com>
2020-01-21 23:59:50 +0100

    Reviews - Delete Refs

--- controllers/posts.js
+++ controllers/posts.js
@@ -117,6 +117,7 @@ module.exports = {
             await cloudinary.v2.uploader.destroy(image.public_id);
         }
         await post.remove();
+        req.session.success = 'Post deleted successfully!';
         res.redirect('/posts');
     }
 }
--- models/post.js
+++ models/post.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const PostSchema = new Schema({
-	title: String,
-	price: String,
-	description: String,
-	images: [ { url: String, public_id: String } ],
-	location: String,
-	coordinates: Array,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	reviews: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Review'
-		}
-	]
+    title: String,
+    price: String,
+    description: String,
+    images: [{ url: String, public_id: String }],
+    location: String,
+    coordinates: Array,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
 });
-
+PostSchema.pre('remove', async function() {
+    await Review.remove({
+        _id: {
+            $in: this.reviews
+        }
+    });
+});
 module.exports = mongoose.model('Post', PostSchema);
