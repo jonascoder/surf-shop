62de3a41be18aaa4adf4417be64dfa0405bac5da
Jonas Rodon <jonascoder@gmail.com>
2020-01-21 23:17:31 +0100

    Reviews - Delete

--- routes/reviews.js
+++ routes/reviews.js
@@ -14,7 +14,7 @@ router.post('/', asyncErrorHandler(reviewCreate));
 router.put('/:review_id', isReviewAuthor, asyncErrorHandler(reviewUpdate));
 
 /* DELETE reviews destroy /posts/:id/reviews/:review_id */
-router.delete('/:review_id', asyncErrorHandler(reviewDestroy));
+router.delete('/:review_id', isReviewAuthor, asyncErrorHandler(reviewDestroy));
 
 
 module.exports = router;
