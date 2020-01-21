d57f74cbff151a791a0c584e2ae8cd9f7b5c49c1
Jonas Rodon <jonascoder@gmail.com>
2020-01-22 00:52:14 +0100

    Clear Rating Button

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -23,3 +23,8 @@ $('.toggle-edit-form').on('click', function() {
     // toggle the visibility of the edit review form
     $(this).siblings('.edit-review-form').toggle();
 });
+
+// Add click listener for clearing of rating from edit/new form
+$('.clear-rating').click(function() {
+    $(this).siblings('.input-no-rate').click();
+});
