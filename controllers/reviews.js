451990ce64392fa9603bbf6c2cebb6be8592e159
Jonas Rodon <jonascoder@gmail.com>
2020-01-21 23:46:14 +0100

    Reviews - Restrict

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -6,7 +6,14 @@ module.exports = {
     // Reviews Create
     async reviewCreate(req, res, next) {
         // find the post by its id
-        let post = await Post.findById(req.params.id);
+        let post = await Post.findById(req.params.id).populate('reviews').exec();
+        let haveReviewed = post.reviews.filter(review => {
+            return review.author.equals(req.user._id)
+        }).length;
+        if (haveReviewed) {
+            req.session.error = 'Sorry, you can only create one review per post.';
+            return res.redirect(`/posts/${post.id}`);
+        }
         // create the review
         req.body.review.author = req.user._id;
         let review = await Review.create(req.body.review);
