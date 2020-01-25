ac4709eeed81fa71feb485c104563bb2262ac240
Jonas Rodon <jonascoder@gmail.com>
2020-01-25 01:05:53 +0100

    Create profile and add recent posts

--- controllers/index.js
+++ controllers/index.js
@@ -54,5 +54,10 @@ module.exports = {
     getLogout(req, res, next) {
         req.logout();
         res.redirect('/');
+    },
+    // GET /profile
+    async getProfile(req, res, next) {
+        const posts = await Post.find().where('author').equals(req.user._id).limit(10).exec();
+        res.render('profile', { posts });
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { 
-	landingPage,
-	getRegister,
-	postRegister,
-	getLogin,
-	postLogin,
-	getLogout } = require('../controllers');
-const { asyncErrorHandler } = require('../middleware')
+const {
+    landingPage,
+    getRegister,
+    postRegister,
+    getLogin,
+    postLogin,
+    getLogout,
+    getProfile
+} = require('../controllers');
+const { asyncErrorHandler, isLoggedIn } = require('../middleware')
 
 /* GET home/landing page. */
 router.get('/', asyncErrorHandler(landingPage));
@@ -28,33 +30,30 @@ router.post('/login', asyncErrorHandler(postLogin));
 router.get('/logout', getLogout);
 
 /* GET /profile */
-router.get('/profile', (req, res, next) => {
-  res.send('GET /profile');
-});
-
+router.get('/profile', isLoggedIn, asyncErrorHandler(getProfile));
 /* PUT /profile/:user_id */
 router.put('/profile/:user_id', (req, res, next) => {
-  res.send('PUT /profile/:user_id');
+    res.send('PUT /profile/:user_id');
 });
 
 /* GET /forgot */
 router.get('/forgot', (req, res, next) => {
-  res.send('GET /forgot');
+    res.send('GET /forgot');
 });
 
 /* PUT /forgot */
 router.put('/forgot', (req, res, next) => {
-  res.send('PUT /forgot');
+    res.send('PUT /forgot');
 });
 
 /* GET /reset/:token */
 router.get('/reset/:token', (req, res, next) => {
-  res.send('GET /reset/:token');
+    res.send('GET /reset/:token');
 });
 
 /* PUT /reset/:token */
 router.put('/reset/:token', (req, res, next) => {
-  res.send('PUT /reset/:token');
+    res.send('PUT /reset/:token');
 });
 
 module.exports = router;
