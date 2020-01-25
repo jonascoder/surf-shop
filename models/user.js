aed0b2878469bc788738145e7ec56bf8af40c39d
Jonas Rodon <jonascoder@gmail.com>
2020-01-25 18:45:35 +0100

    Forgot Password / Reset

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
-
 const UserSchema = new Schema({
-    username: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
     image: {
         secure_url: { type: String, default: '/images/default-profile.jpg' },
         public_id: String
-    }
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
